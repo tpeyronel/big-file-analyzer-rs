21e65f59c2c152cb14f7dfad89db7c548373c60b
Tomás Peyronel <tpeyronel@gmail.com>
2024-05-05 11:47:38 -0300

    read with retry

---
+++ src/file.rs
@@ -0,0 +1,28 @@
+use std::io::{self, Read};
+
+pub trait ReadRetry {
+    fn read_with_retry(&mut self, buf: &mut [u8]) -> io::Result<()>;
+}
+
+impl<T: Read> ReadRetry for T {
+    fn read_with_retry(&mut self, buf: &mut [u8]) -> io::Result<()> {
+        let mut bytes_read = 0;
+        while bytes_read < buf.len() {
+            match self.read(&mut buf[bytes_read..]) {
+                Ok(bytes) => {
+                    if bytes == 0 {
+                        break;
+                    }
+
+                    bytes_read += bytes;
+                }
+                Err(err) => match err.kind() {
+                    io::ErrorKind::Interrupted => continue,
+                    _ => return Err(err),
+                },
+            }
+        }
+
+        Ok(())
+    }
+}
